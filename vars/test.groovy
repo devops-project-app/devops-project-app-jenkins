3573afe501a2b8ddea05c3686aa05fd003079bc4
ngochung1809 <89231997+ngocHung2000@users.noreply.github.com>
2023-09-26 22:28:15 +0700

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,7 +1,23 @@
 def call(){
   node {
-    stage('Testing') {
-      echo 'Hello world'
+    def nodejsInstallation = tool name: 'nodejs', type: 'jenkins.plugins.nodejs.tools.NodeJSInstallation'
+    env.PATH = "${nodejsInstallation}/bin:${env.PATH}"
+    
+    stage('Checkout') {
+            checkout([$class: 'GitSCM',
+                    branches: [[name: '*/master']],
+                    doGenerateSubmoduleConfigurations: false,
+                    extensions: [],
+                    submoduleCfg: [],
+                    userRemoteConfigs: [[url: 'https://github.com/devops-project-app/devops-project-app-frontend.git']]])
+    }
+
+    stage('Install') {
+      sh 'npm install'
+    }
+
+    stage ('Run & Build') {
+      sh 'npm run build'
     }
   }
 }
